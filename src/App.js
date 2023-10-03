81488bf0fdd7d08bc854d3548401f96ade6d24ff
Luciano Kohler da Silva <lucianokohlerdasilva@gmail.com>
2023-10-03 14:35:37 -0300

    atualização

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import api from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    filmes: [],
+    query: ""
+  }
+
+  // async componentDidMount() {
+  //   const response = await api.get('star%20wars');
+  //   this.setState({ filmes: response.data });
+  // }
+
+  handleInputChange = e => this.setState({ query: e.target.value });
+
+  async fetchFilmes(query) {
+    const response = await api.get(query);
+    this.setState({ filmes: response.data });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { query } = this.state;
+    this.fetchFilmes(query);
+  }
+
+  render() {
+    const {filmes, query} = this.state;
+    
+    return (
+      <>
+      <div className='h-full'>
+        <h1>Listar filmes</h1>
+        <form>
+          <label htmlFor="">Pesquisar por Título</label>
+          <input type="text" name="query" id="query" value={query} onChange={this.handleInputChange} />
+          <button type="submit" onClick={this.handleSubmit}>Pesquisar</button>
+        </form>
+      </div>
+      <div>
+        <h1>Star Filmes</h1>
+        {filmes.map(filme => (
+          <li key={filme.show.id}>            
+            <h2 style={{display: 'inline'}}>
+              <strong>Título: </strong>
+              {filme.show.name}
+            </h2>
+            <p>
+              {filme.show.url}
+            </p>
+            <p>
+              {filme.show.image && filme.show.image.medium ? ( <img src={filme.show.image.medium} height={200} /> ) : (<span>Imagem não disponível</span>)}
+            </p>
+          </li>
+        ))}
+      </div>
+      </>
+    );
+  }
 }
 
 export default App;
